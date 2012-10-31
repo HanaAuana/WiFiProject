cc34a571de44fc1cf24ac03f6d5845ea37532be2
Alex King <aps.king@gmail.com>
2012-10-30 22:04:54 -0700

    TEST- Created a Packet Class
    -Alex

---
+++ src/wifi/Packet.java
@@ -0,0 +1,13 @@
+package wifi;
+
+/**
+ * @author Alexander  King & Michael Lim
+ * 
+ * The Packet class wraps all neccesary information that is required for 
+ * a transmission for the 802.11~ spec.
+ * 
+ */
+
+public class Packet {
+
+}
