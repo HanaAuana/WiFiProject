8d0a31d880adc5782d0f81227898297ce232a2dd
Alex King <aps.king@gmail.com>
2012-11-04 23:46:47 -0800

    Update Main tests

--- src/wifi/Main.java
+++ src/wifi/Main.java
@@ -8,14 +8,16 @@ public class Main {
 	}
 	
 	public static void testPacketInOut(){
-		int type = 1;
-		short seq = 0;
+		int type = 7;
+		short seq = 1;
 		short dest = 534;
 		short src = 256;
+		int retry = 1;
 		byte[] data = {1, 1};
 		byte[] crc = {1,2,3,1};
 		System.out.println("Data In:");
 		System.out.println("\t Type:" + type);
+		System.out.println("\t Retry:" + retry);
 		System.out.println("\t SeqNum:" + seq);
 		System.out.println("\t DestAddr:" + dest);
 		System.out.println("\t SrcAddr:" + src);
@@ -32,11 +34,13 @@ public class Main {
 		
 		Packet test = new Packet(type, seq, dest, src, data, crc);
 		
-		System.out.println("Data Out:");
-		System.out.println("\t Type:" + test.getFrameType());
-		System.out.println("\t SeqNum:" + test.getSeqNum());
-		System.out.println("\t DestAddr:" + test.getDestAddr());
-		System.out.println("\t SrcAddr:" + test.getSrcAddr());
+		System.out.println("Data Out: ");
+		System.out.println("\t Type: " + test.getFrameType());
+		test.setRetry(true);
+		System.out.println("\t Retry: " + test.isRetry());
+		System.out.println("\t SeqNum: " + test.getSeqNum());
+		System.out.println("\t DestAddr: " + test.getDestAddr());
+		System.out.println("\t SrcAddr: " + test.getSrcAddr());
 		System.out.print("\t Data: ");
 		for(int i = 0;i <test.getData().length; i++){
 			System.out.print(test.getData()[i] + " ");
